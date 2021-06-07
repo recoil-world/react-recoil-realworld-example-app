e1abd4ea58575493d42742519109c86f9fa020af
jho2301 <jho2301@gmail.com>
2021-06-07 11:09:45 +0900

    chore: setup typescript-react environment

---
+++ webpack.config.ts
@@ -0,0 +1,55 @@
+import webpack from 'webpack';
+import webpackDevServer from 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+interface WebpackConfig extends webpack.Configuration {
+  devServer?: webpackDevServer.Configuration;
+}
+
+const webpackConfig: WebpackConfig = {
+  mode: 'development',
+  entry: path.resolve('src', 'index.tsx'),
+  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+  output: {
+    filename: '[contenthash].bundle.js',
+    path: path.resolve('dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /(\.png|\.jpe?g|\.svg)/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve('public'),
+    compress: true,
+    hot: true,
+    historyApiFallback: true,
+    open: true,
+    // localhost Refused to connect
+    // https://github.com/webpack/webpack-dev-server/issues/1347#issuecomment-407008054
+    useLocalIp: true,
+    host: '0.0.0.0',
+  },
+};
+
+export default webpackConfig;
